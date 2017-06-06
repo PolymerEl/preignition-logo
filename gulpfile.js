15698ed3a0c4493e8c38d170feee2ba12faeb855
christophe-g <christophe.geiser@gmail.com>
2017-06-06 15:43:28 +0200

    added png logo as a result of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,20 @@
 
 // Include Gulp & tools we'll use
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+// var $ = require('gulp-load-plugins')();
+var rename = require('gulp-rename');
 var svgmin = require('gulp-svgmin');
+var svg2png = require('gulp-svg2png');
+// var imageResize = require('gulp-image-resize');
 
 gulp.task('svg', function() {
   // return gulp.src(['app/images/logo/working.svg' ,'app/images/logo/working-above.svg','app/images/logo/working-above2.svg', 'app/images/logo/working-online.svg', 'app/images/logo/working-round.svg', 'app/images/logo/working-round-2.svg', 'app/images/logo/working-round-3.svg', 'app/images/logo/working-round-invert.svg'])
   // return gulp.src(['app/images/logo/**.svg', 'images/infographics/**.svg', ])
   // return gulp.src([ 'images/infographics/**.svg', ])
-  return gulp.src([ 'dev/**.svg', ])
+  return gulp.src([ 'src/**.svg', ])
     .pipe(svgmin({
       plugins: [{
-        removeAttrs: { attrs: '(fill|stroke|opacity|id|letter-spacing|color|overflow|stroke-width|fill-rule|fill-opacity)' }
+        removeAttrs: { attrs: '(fill|stroke|id|letter-spacing|color|overflow|stroke-width|fill-rule|fill-opacity)' }
       }, {
         convertPathData: {
           // removeUseless: false,
@@ -25,5 +28,36 @@ gulp.task('svg', function() {
         }
       }]
     }))
-    .pipe(gulp.dest('svgmin'));
+    .pipe(gulp.dest('svg'));
 });
+
+var imageResizeTask = function(size) { 
+// gulp.task('svg2png', function (size) {
+   return  gulp.src('svg/**.svg')
+        .pipe(svg2png({
+          width : size
+        }))
+        .pipe(rename(function (path) { path.basename += ('_' + size); }))
+        .pipe(gulp.dest('img'));
+      };
+// });
+
+//Image Resize
+gulp.task('resize', function() {
+  return [10, 32, 72, 96, 144, 152, 192, 384, 512, 100, 200, 400, 600, 1000].map(function(s) {
+    return imageResizeTask(s);
+  });
+});
+ 
+// gulp.task('resize', function () {
+//   return gulp.src('svgmin/**.svg')
+//     .pipe(imageResize({
+//       gm: 'white',
+//       width : 100,
+//       height : 100,
+//       format: 'png',
+//       // crop : true,
+//       // upscale : false
+//     }))
+//     .pipe(gulp.dest('img'));
+// });
